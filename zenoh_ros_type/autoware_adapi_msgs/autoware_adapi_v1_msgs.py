be7fa26f0ab0afc9a20ce81b182b0af2377c173a
Leann Hsu <leann9001@gmail.com>
2025-11-07 17:07:14 +0800

    Add VehicleStatus in adapi msgs

--- zenoh_ros_type/autoware_adapi_msgs/autoware_adapi_v1_msgs.py
+++ zenoh_ros_type/autoware_adapi_msgs/autoware_adapi_v1_msgs.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
 
 from pycdr2 import Enum, IdlStruct
-from pycdr2.types import int64, sequence, uint16
+from pycdr2.types import float64, int64, sequence, uint8, uint16
 
 from ..common_interfaces.geometry_msgs import AccelWithCovarianceStamped, Pose, PoseWithCovarianceStamped, TwistWithCovarianceStamped
 from ..common_interfaces.std_msgs import Header
 from ..geographic_info.geographic_msgs import GeoPointStamped
+from ..rcl_interfaces.builtin_interfaces import Time
 
 
 @dataclass
@@ -86,3 +87,43 @@ class SetRoutePointsResponse(IdlStruct, typename='SetRoutePointsResponse'):
 @dataclass
 class ClearRouteResponse(IdlStruct, typename='ClearRouteResponse'):
     status: ResponseStatus
+
+
+@dataclass
+class Gear(IdlStruct, typename='Gear'):
+    class STATUS(Enum):
+        UNKNOWN = 0
+        NEUTRAL = 1
+        DRIVE = 2
+        REVERSE = 3
+        PARK = 4
+        LOW = 5
+
+    status: uint8
+
+
+class TurnIndicators(IdlStruct, typename='TurnIndicators'):
+    class STATUS(Enum):
+        UNKNOWN = 0
+        DISABLE = 1
+        LEFT = 2
+        RIGHT = 3
+
+    status: uint8
+
+
+class HazardLights(IdlStruct, typename='HazardLights'):
+    class STATUS(Enum):
+        UNKNOWN = 0
+        DISABLE = 1
+        ENABLE = 2
+
+    status: uint8
+
+
+class VehicleStatus(IdlStruct, typename='VehicleStatus'):
+    stamp: Time
+    gear: Gear
+    turn_indicators: TurnIndicators
+    hazard_lights: HazardLights
+    steering_tire_angle: float64
